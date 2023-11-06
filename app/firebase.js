9908eae8f0eaf2b1ee6d27c2e2b4042564ae2f2c
Ryan Farrar <rfazorla@gmail.com>
2023-11-06 08:48:51 +0000

    Database testing finished, ready 2 impliment cards

--- app/firebase.js
+++ app/firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore, collection } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -18,5 +19,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const database = getFirestore();
+// const POIs = collection(database, "POIs");
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
+export const POIs = collection(database, "POIs");
+export const Cities = collection(database, "Cities");
--- app/team/page.jsx
+++ app/team/page.jsx
@@ -1,8 +1,25 @@
 "use client";
 import React from "react";
+import { Cities } from "../firebase";
+import { getDocs } from "firebase/firestore";
 
-function page() {
-  return <div>page</div>;
+async function page() {
+  let POI = [];
+  await getDocs(Cities).then((snapshot) => {
+    snapshot.docs.forEach((doc) => {
+      POI.push({ ...doc.data(), id: doc.id });
+    });
+  });
+
+  return (
+    <>
+      <div>
+        {POI.map((item) => (
+          <h1>City: {item.City}</h1>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default page;
