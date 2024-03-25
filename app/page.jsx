99e59c78762a5ba3325f057f9b77dc4ff59f5561
Ryan Farrar <rfazorla@gmail.com>
2024-03-25 22:25:25 +0000

    Testing commit to both gits

--- app/page.jsx
+++ app/page.jsx
@@ -13,6 +13,7 @@ const Home = () => {
   }
   return (
     <>
+      {/* wow */}
       <div className="h-[calc(100vh-130px)] overflow-auto flex flex-col items-center justify-center">
         <div className="text-center">
           <h1 className="text-4xl font-bold text-gray-700 ">DayOut to</h1>
